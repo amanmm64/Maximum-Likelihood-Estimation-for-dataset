95ce20c69e4d539c97b891e59218d8e0666982b7
Aman Kumar <44062955+axelblaze11@users.noreply.github.com>
2020-06-01 15:57:24 +0530

    Update mle.py

--- mle.py
+++ mle.py
@@ -4,25 +4,38 @@ from scipy.stats import norm
 import math 
 import array
 import matplotlib.pyplot as plt
-def MLE(X):	
-	mean=X.mean()
-	std=X.std()
-	normal=norm.cdf(X,loc=mean,scale=std)
-	df=pd.DataFrame(normal)
-	lh=np.product(df)
-	llh=np.log(lh)
-	median=llh.median()
-	llh=abs(llh)
-	median=abs(median)
-	c=llh.count()
-	n=int(c)
-	a=[]
-	for i in range(1, n+1):
-		a.append(i)
-	plt.plot(a,lh,marker='o')
-	plt.xticks(np.arange(0,20,1))
-	plt.show()
-	plt.plot(a,llh, marker='o')
-	plt.xticks(np.arange(0, 20, 1))
-	plt.show() 
-	return 
+# Here X is the dataset.
+mean=X.mean()
+std=X.std()
+normal=norm.cdf(X,loc=mean,scale=std)
+df=pd.DataFrame(normal)
+lh=np.product(df)
+llh=np.log(lh)
+median=llh.median()
+llh=abs(llh)
+median=abs(median)
+c=llh.count()
+n=int(c)
+a=[]
+b=[]
+k=1
+for i in range(1, n+1):
+	a.append(i)
+if c>10:
+   ax=5
+else:
+   ax=1     
+'''plt.plot(a,lh,marker='o')
+plt.xticks(np.arange(0,c+ax,ax))
+plt.show()'''
+plt.plot(a,llh, marker='o')
+plt.xticks(np.arange(0, c+ax,ax))
+plt.show()
+for j in range(1, n):
+    if llh[j]>median:
+        m=a[j]
+        b.insert(k,m)
+        k=k+1
+print(k-1, " features/attributes selected")
+print(b, "indices are selected")
+X=dataset.iloc[:,b]
